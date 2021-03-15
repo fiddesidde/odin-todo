c89d6d7ce8e30fac266e8f35d0a9998305e28f11
fiddesidde <fredrik.mellberg@gmail.com>
2021-03-15 17:47:50 +0100

    Add eslint

--- src/index.js
+++ src/index.js
@@ -3,66 +3,71 @@ import { showListItems, showLists } from './dom';
 export const allLists = [];
 
 const TodoItem = (title, description, dueDate = new Date(), priority = 2) => {
-    let done = false;
-    let prio = priority;
-    const id = Math.round(Math.random() * 10000 + 1);
-    const getTitle = () => title;
-    const getDescription = () => description;
-    const getDueDate = () => dueDate;
-    const getPrio = () => prio;
-    const setPrio = num => (prio = num);
-    const getId = () => id;
-    const toggleComplete = () => (done = !done);
-    const isComplete = () => done;
+  let done = false;
+  let prio = priority;
+  const id = Math.round(Math.random() * 10000 + 1);
+  const getTitle = () => title;
+  const getDescription = () => description;
+  const getDueDate = () => dueDate;
+  const getPrio = () => prio;
+  const setPrio = num => {
+    prio = num;
+  };
+  const getId = () => id;
+  const toggleComplete = () => {
+    done = !done;
+  };
+  const isComplete = () => done;
 
-    return {
-        getTitle,
-        getDescription,
-        getDueDate,
-        getPrio,
-        setPrio,
-        getId,
-        toggleComplete,
-        isComplete,
-    };
+  return {
+    getTitle,
+    getDescription,
+    getDueDate,
+    getPrio,
+    setPrio,
+    getId,
+    toggleComplete,
+    isComplete,
+  };
 };
 
 const TodoList = name => {
-    const id = Math.round(Math.random() * 10000 + 1);
-    let todoItems = [];
-    const getName = () => name;
-    const getId = () => id;
-    const addTodoItem = TodoItem => todoItems.push(TodoItem);
-    const deleteTodoItem = id =>
-        (todoItems = todoItems.filter(item => id !== item.getId()));
-    const getListItems = () => todoItems;
+  const id = Math.round(Math.random() * 10000 + 1);
+  let todoItems = [];
+  const getName = () => name;
+  const getId = () => id;
+  const addTodoItem = TodoItem => todoItems.push(TodoItem);
+  const deleteTodoItem = id => {
+    todoItems = todoItems.filter(item => id !== item.getId());
+  };
+  const getListItems = () => todoItems;
 
-    return {
-        getName,
-        addTodoItem,
-        deleteTodoItem,
-        getListItems,
-        getId,
-    };
+  return {
+    getName,
+    addTodoItem,
+    deleteTodoItem,
+    getListItems,
+    getId,
+  };
 };
 
 export const createTodoList = name => {
-    const list = TodoList(name);
-    allLists.push(list);
-    showLists();
-    return list;
+  const list = TodoList(name);
+  allLists.push(list);
+  showLists();
+  return list;
 };
 
 // TEST STUFF
 
 const testList = createTodoList('TestList');
-for (let i = 0; i < 3; i++) {
-    testList.addTodoItem(TodoItem(`Title${i}`, `description${i}`));
+for (let i = 0; i < 3; i += 1) {
+  testList.addTodoItem(TodoItem(`Title${i}`, `description${i}`));
 }
 
 const testList2 = createTodoList('New testList');
 testList2.addTodoItem(
-    TodoItem('awsome thing', 'yes do this awsome thing', '2023-01-25', 1)
+  TodoItem('awsome thing', 'yes do this awsome thing', '2023-01-25', 1)
 );
 
 // TEST STUFF END
